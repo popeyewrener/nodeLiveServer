f1641f12cf296645984645735e213f4b7a56a806
System Administrator <root@Chaitanyas-MacBook-Air.local>
2023-08-25 15:41:30 +0530

    socket test roomCreate

--- handlers/socketHandler.js
+++ handlers/socketHandler.js
@@ -1,7 +1,26 @@
+const { createRoom } = require("../services/roomService");
+
 handleSocket = (io,socket)=>{
     socket.on("sendMessage", (message)=>{
+        io.emit("recieveMessage", message);
+    });
+    socket.on("roomCreate", async (req)=>{
+        const reqBody = req.body;
+        const roomname = reqBody.roomname;
+        const agoraToken = reqBody.agoraToken;
+        const desc = reqBody.desc;
+        const imageUrl = reqBody.imageUrl;
+        console.log(roomname);
+        console.log(agoraToken)
+        
+        const room = await createRoom(roomname,agoraToken,desc,imageUrl);
+        //console.log(userId);
+        console.log(room);
+        socket.emit("roomDetail", room)
         
     });
+
+    
     socket.on("joinLive", (message)=>{
         
     });
